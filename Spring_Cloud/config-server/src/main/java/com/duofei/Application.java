4ba052a0b683a3be7cc2a176fca90e777d5b32fa
xiaofei007 <324798067@qq.com>
2019-10-17 20:25:08 +0800

    spring-cloud配置中心学习

---
+++ Spring_Cloud/config-server/src/main/java/com/duofei/Application.java
@@ -0,0 +1,19 @@
+package com.duofei;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author duofei
+ * @date 2019/10/17
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
