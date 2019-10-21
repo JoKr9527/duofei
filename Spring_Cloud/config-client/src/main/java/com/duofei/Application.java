8d6fba497132f31699f5fd748c743b2ad2b6b3f0
xiaofei007 <324798067@qq.com>
2019-10-21 09:02:57 +0800

    spring_cloud配置中心完成学习

--- Spring_Cloud/config-client/src/main/java/com/duofei/Application.java
+++ Spring_Cloud/config-client/src/main/java/com/duofei/Application.java
@@ -3,16 +3,12 @@ package com.duofei;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author duofei
- * @date 2019/10/17
+ * @date 2019/10/18
  */
-@EnableConfigServer
 @SpringBootApplication
-@EnableWebSecurity
 public class Application {
 
     public static void main(String[] args) {
