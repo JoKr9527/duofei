283461a558963cc0c9460bc7d89851d04ccb7337
Jokr9527 <324798067@qq.com>
2020-07-26 17:45:14 +0800

    代码提交

--- duofei_study/src/main/java/com/duofei/synchron/Solver.java
+++ duofei_study/src/main/java/com/duofei/synchron/Solver.java
@@ -2,6 +2,8 @@ package com.duofei.synchron;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * CyclicBarrier
@@ -15,14 +17,15 @@ public class Solver {
     final CyclicBarrier barrier;
 
     public static void main(String[] args) {
-        Solver solver = new Solver(new float[5][2]);
+        Solver solver = new Solver(new float[2][2]);
 
         for (int i = 0; i < solver.N; i++) {
+            String 我和 = "js";
             final Thread thread = new Thread(solver.new Worker(i));
             thread.start();
-            System.out.println(System.currentTimeMillis() + ": 打断" + thread.getName());
+            //System.out.println(System.currentTimeMillis() + ": 打断" + thread.getName());
             // 测试线程打断
-            thread.interrupt();
+            //thread.interrupt();
         }
     }
 
@@ -35,10 +38,15 @@ public class Solver {
 
         @Override
         public void run() {
-            System.out.println("我处理完成第" + myRow + "行");
+            System.out.println(Thread.currentThread().getName() + ": " + "我处理完成第" + myRow + "行");
             try {
-                final int await = barrier.await();
-                System.out.println("我完成了" + myRow+ "行，我是第" + await + "个到达屏障处的");
+                int await = -1;
+                try {
+                    await = barrier.await(1000, TimeUnit.NANOSECONDS);
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ": " + "我完成了" + myRow+ "行，我是第" + await + "个到达屏障处的");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (BrokenBarrierException e) {
@@ -52,7 +60,7 @@ public class Solver {
         data = matrix;
         N = matrix.length;
         barrier = new CyclicBarrier(N, ()->{
-            System.out.println("行数据合并");
+            System.out.println(Thread.currentThread().getName() + ": " + "行数据合并");
         });
     }
 }
